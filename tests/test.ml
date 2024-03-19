1e2c6bbb9567afa69e668ccece75a44cac0db897
Andreas <andreas.kruehlmann@siemens.com>
2024-03-19 17:30:02 +0100

    Segment tests

--- tests/test.ml
+++ tests/test.ml
@@ -2,5 +2,8 @@ let () =
   Lwt_main.run
   @@ Alcotest_lwt.run
        "API"
-       [ "get", [ Alcotest_lwt.test_case "advertisements" `Quick (fun _ () -> Api.test_get_advertisements ()) ] ]
+       [ ( "get"
+         , [ Alcotest_lwt.test_case "advertisements" `Quick (fun _ () -> Test_cases.Api.test_get_advertisements ()) ]
+         )
+       ]
 ;;
