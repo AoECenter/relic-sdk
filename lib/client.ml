c154868febd11b3b83f707d5fc194020bbca7f15
Andreas <andreas.kruehlmann@siemens.com>
2024-03-19 17:51:47 +0100

    Add error status logging for now

--- lib/client.ml
+++ lib/client.ml
@@ -15,7 +15,10 @@ let get_json url =
     let* body = Cohttp_lwt.Body.to_string body in
     let json = Yojson.Basic.from_string body in
     Lwt.return @@ Some json
-  else Lwt.return None
+  else
+    (* Find out what to do with this later. What's the return type? Do we have Result<a,b>? *)
+    let* _ = Lwt_io.printl (Printf.sprintf "HTTP Error: %s" (Cohttp.Code.string_of_status status)) in
+    Lwt.return None
 ;;
 
 let get ?requester endpoint client =
