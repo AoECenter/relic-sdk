73b6f442e765c9ee3146014089dcef1ece3831ad
Andreas <andreas.kruehlmann@siemens.com>
2024-04-09 17:17:26 +0200

    Add optional arg and remove cflag warnings

--- lib/compression/domain.ml
+++ lib/compression/domain.ml
@@ -1,4 +1,4 @@
-let decompress_base64 base64_str =
+let decompress_base64 ?(buffer_size_factor = 12) base64_str =
   let compressed_data = Base64.decode_exn base64_str in
   let compressed_len = String.length compressed_data in
   let compressed_ba = Bigarray.Array1.create Bigarray.char Bigarray.c_layout compressed_len in
@@ -8,14 +8,13 @@ let decompress_base64 base64_str =
   let inflate_state = Zlib.create_inflate () in
   inflate_state.Zlib.in_buf <- compressed_ba;
   inflate_state.Zlib.in_len <- compressed_len;
-  let output_len = compressed_len * 16 in
+  let output_len = compressed_len * buffer_size_factor in
   let output_ba = Bigarray.Array1.create Bigarray.char Bigarray.c_layout output_len in
   inflate_state.Zlib.out_buf <- output_ba;
   inflate_state.Zlib.out_len <- output_len;
   let status = Zlib.flate inflate_state Zlib.Finish in
   match status with
   | Zlib.Ok | Zlib.Stream_end ->
-    (* Successful decompression *)
     let result_len = output_len - inflate_state.Zlib.out_len in
     let result = String.init result_len (fun i -> Bigarray.Array1.get output_ba i) in
     Some result
