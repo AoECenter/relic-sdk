81a1c8be9ad9dad8972eff68b7261de96e61eb98
Andreas Krühlmann <andreas@kruhlmann.dev>
2024-05-24 14:20:44 +0200

    Fix form

--- lib/form.ml
+++ lib/form.ml
@@ -1,18 +1,20 @@
 open Lwt.Syntax
-open Cohttp_lwt
+open Cohttp_lwt_unix
 
 let post (url : Uri.t) (form_data : (string * string) list) =
-  let headers = Header.init_with "Content-Type" "application/x-www-form-urlencoded" in
-  let body = Uri.encoded_of_query form_data in
-  let* resp, body = Client.post ~headers ~body:(Body.of_string body) url in
-  let status = Response.status resp in
-  if Code.code_of_status status = 200
+  let headers = Cohttp.Header.init_with "Content-Type" "application/x-www-form-urlencoded" in
+  let body = Uri.encoded_of_query (List.map (fun (k, v) -> k, [ v ]) form_data) in
+  let* resp, body = Client.post ~headers ~body:(Cohttp_lwt.Body.of_string body) url in
+  let status = Cohttp.Response.status resp in
+  if Cohttp.Code.code_of_status status = 200
   then
-    let* body = Body.to_string body in
+    let* body = Cohttp_lwt.Body.to_string body in
     let json = Yojson.Basic.from_string body in
     Lwt.return (Some json)
   else (
     let url_str = Uri.to_string url in
-    let* _ = Lwt_io.printl (Printf.sprintf "HTTP Error: %s for URL: %s" (Code.string_of_status status) url_str) in
+    let* _ =
+      Lwt_io.printl (Printf.sprintf "HTTP Error: %s for URL: %s" (Cohttp.Code.string_of_status status) url_str)
+    in
     Lwt.return None)
 ;;
