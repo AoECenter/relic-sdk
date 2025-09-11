a321379a893ddd43bef6b90c4ac0cd8097a36f56
Robin Björklin <robin.bjorklin@gmail.com>
2025-09-10 18:20:19 -0700

    fix: requester type with other .mli files

--- lib/api/community/leaderboard.mli
+++ lib/api/community/leaderboard.mli
@@ -1,21 +1,17 @@
 val get_available_leaderboards
   :  Data.Game.t
   -> string
-  -> (Uri.t -> Yojson.Basic.t option Lwt.t)
+  -> Data.Requester.Json.t
   -> Models.Response.Community.Leaderboard.t option Lwt.t
 
-val get
-  :  Data.Game.t
-  -> string
-  -> (Uri.t -> Yojson.Basic.t option Lwt.t)
-  -> Models.Response.Community.Leaderboard.t option Lwt.t
+val get : Data.Game.t -> string -> Data.Requester.Json.t -> Models.Response.Community.Leaderboard.t option Lwt.t
 [@@deprecated "Use the more clearly named 'get_available_leaderboards' instead"]
 
 val get_avatar
   :  ?profile_ids:int list
   -> Data.Game.t
   -> string
-  -> (Uri.t -> Yojson.Basic.t option Lwt.t)
+  -> Data.Requester.Json.t
   -> Models.Response.Community.Avatar_stat.t option Lwt.t
 
 val get_leaderboard_2
@@ -26,19 +22,19 @@ val get_leaderboard_2
   -> ?count:int
   -> Data.Game.t
   -> string
-  -> (Uri.t -> Yojson.Basic.t option Lwt.t)
+  -> Data.Requester.Json.t
   -> Models.Response.Community.Leaderboard2.t option Lwt.t
 
 val get_recent_match_history
   :  profile_ids:int list
   -> Data.Game.t
   -> string
-  -> (Uri.t -> Yojson.Basic.t option Lwt.t)
+  -> Data.Requester.Json.t
   -> Models.Response.Community.Recent_match_history.t option Lwt.t
 
 val get_personal_stat
   :  profile_ids:int list
   -> Data.Game.t
   -> string
-  -> (Uri.t -> Yojson.Basic.t option Lwt.t)
+  -> Data.Requester.Json.t
   -> Models.Response.Community.Personal_stat.t option Lwt.t
